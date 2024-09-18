ba91736e17a1c213a587e6a6dcb80758ad586f2c
trev <trev@trevs-MacBook-Air.local>
2024-09-18 13:02:17 -0700

    style, feat - create instructions page, style instructions page

---
+++ src/components/instructions/ConnectWallet.tsx
@@ -0,0 +1,49 @@
+import styles from "./ConnectWallet.module.css";
+import metaMask from "../../assets/metamask.webp";
+
+export const ConnectWallet = ({onStateChange}) => {
+    const handleClick = () => {
+        onStateChange((prev: any) => !prev);
+    }
+
+    return (
+        <>
+        <h1 className={styles.backButton} onClick={handleClick}>⬅️</h1>
+        <div className={styles.container}>
+            <div className={styles.titleContainer}>
+        <h1 className={styles.title}>Setting Up Your Wallet</h1>
+        </div>
+        <div className={styles.linkContainer}>
+        <a href="https://metamask.io/" target="_blank"> <p className={styles.titleLink}>Quick Download</p> </a><img src={metaMask} alt="metamask logo" width={20}/></div>
+            <section className={styles.section}>
+            <h2 className={styles.sectionTitle}>How A Wallet Works</h2>
+            <p>To connect with Dapps, you must do so through a wallet. Your wallet will act as your identity, allow you access to the web3 space.</p>
+            <p>Your identity remains anonymous and you will be identified only through your public address.</p>
+            <p>Information stored from interactions you make on Dapps will be accessable through your public address, ensuring you have access to data you store on-chain</p>
+            </section>
+            <section className={styles.section}>
+            <h2 className={styles.sectionTitle}>How To Interact With A Dapp</h2>
+            <p>From your laptop, you may interact with Dapps by downloading a wallet exstension that will connect you to any Dapp.</p>
+            <p>Upon landing on A dapps site, it will often ask you for permission to connect your wallet to the Dapp. This is crucial to ensure data related to your interactions are accosiated with your wallets address. After granting permission, you are free to interact with the dapp</p>
+            </section>
+            <section className={styles.section}>
+            <h2 className={styles.sectionTitle}>Costs Of Interacting With Dapps</h2>
+            <p>Since data storage and retreival cost computational effort, a fee is accrued from interacting with the blockchain. This is known as gas and is paid with ETH.</p>
+            <p>Interacting and updating the state of a dapp will cost gas, simple reads do not. Visit<a href="https://ethereum.foundation/" target="_blank">Ethereum</a>official site to learn more about the technology</p>
+            </section>
+            <section className={styles.section}>
+            <h2 className={styles.sectionTitle}>Benefits Of Having A Wallet</h2>
+            <p>Interacting with Dapps through your own wallet means that you have complete control over your funds and data stored on chain.</p>
+            <p>With this comes great responsibility, as you will be in control of your keys. This means if you loose keys to your wallet, you will not be able to recover anything. Be sure to keep your keys safe!</p>
+
+            </section>
+            <section className={styles.section}>
+            <h2 className={styles.sectionTitle}>Recommended Wallet</h2>
+            <p>Metamask is the most widely used wallet in web3. We recommend downloading the browser exention so you can have quick access to all Dapps.</p>
+            <a href="https://metamask.io/" target="_blank">Official Site</a>
+            </section>
+            
+        </div>
+        </>
+    )
+}
--- src/components/instructions/Instructions.tsx
+++ src/components/instructions/Instructions.tsx
@@ -1,20 +1,31 @@
+import {useState} from "react";
 import styles from "./Instructions.module.css"; // Assuming you have a CSS module for styling
+import { ConnectWallet } from "./ConnectWallet";
 
 const Instructions: React.FC = () => {
+    const [isLinkActive, setIsLinkActive]  = useState(false);
+    const handleConnectWallet = () => {
+        setIsLinkActive((prev: any) => !prev);
+
+
+    }
     return (
-        <div className={styles.container}>
+        <>
+        {isLinkActive == true ? (<ConnectWallet onStateChange={setIsLinkActive} />) : (<></>)}
+        <div className={`${styles.container} ${isLinkActive ? styles.hide : styles.show}`}>
             <div className={styles.introContainer}>
-            <h1>Instructions</h1>
+            <h1 className={styles.title}>Instructions</h1>
             <p>Welcome to the Instructions page. Here you will find guidelines and tips on how to use the application.</p>
             </div>
             <ul>
-                <li>Step 1: Connect your wallet.</li>
-                <li>Step 2: Choose a lottery option.</li>
-                <li>Step 3: Enter the lottery.</li>
-                <li>Step 4: Check the results.</li>
+                <li><strong>Step 1:</strong>Connect your wallet. Dont have a wallet? <a onClick={handleConnectWallet}>Click Me</a></li>
+                <li><strong>Step 2:</strong> Choose a lottery option.</li>
+                <li><strong>Step 3:</strong> Enter the lottery.</li>
+                <li><strong>Step 4:</strong> Check the results.</li>
             </ul>
             <p>If you need further assistance, please contact support.</p>
         </div>
+        </>
     );
 };
 
